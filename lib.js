d9a12ea0b214f01bfbfb10068a609b5bfe54b0ad
Adam Fraser <AJFraser10@GMail.com>
2016-08-17 14:06:01 -0500

    js-curry-assignment-1 completed

--- lib.js
+++ lib.js
@@ -25,8 +25,34 @@ const listedPrice =
 const calculateTotals =
   listings =>
     carts => {
-      // TODO
+
+      return carts.map((cart) => ({customer: cart.customer,total:
+        [cart.items.reduce((total, item) => total +=
+          listings.reduce((previous, listingName) => previous + listedPrice (listingName)(item),0),0)]}))
     }
+
+// const cartNames =
+// ['adam', 'david', 'michael', 'dillon']
+
+// cartNames
+//   .map(cartN =>
+//     cartCheckers
+//       .reduce()(cart, listing) => cart + listing(cartN), `${cartN}`)
+//     )
+//     .forEach(cart => console.log(msg))
+
+//   const cartCheckers =
+//    allCarts
+//    .map(arr => isInCart(arr[0], arr[1]))
+//
+// const isInCart(){
+//   if (cart.items === true) {
+//   cart.items.reduce()(cart, items) => {
+//
+//     }, initial)
+//   }
+// }
+
 
 module.exports = {
   listing,
